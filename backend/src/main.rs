ab773d15d6d327bedbd51b652485dcafa367096b
Nathan Hare <me@laspruca.nz>
2021-11-25 14:25:20 +1300

    Initialise git repo

---
+++ backend/src/main.rs
@@ -0,0 +1,85 @@
+static PROGRAM: &str = r#"import axiod from "https://deno.land/x/axiod/mod.ts";
+
+const api_base = "https://api.cloudflare.com/client/v4";
+
+const headers = {
+  "X-Auth-Email": "nathanhare32@gmail.com",
+  "Authorization": "Bearer " + token,
+  "Content-Type": "application/json",
+};
+
+type Zone = {
+  id: string;
+  name: string;
+};
+
+type Dns = {
+  id: string;
+  name: string;
+  type: string;
+  content: string;
+};
+
+try {
+  const { data: zones } = await axiod.get(api_base + "/zones", { headers });
+
+  const zoneId = zones.result.filter((f: Zone) =>
+    f.name == CF_ZONE
+  ).map((f: Zone) => f.id)[0];
+
+  const { data: dns } = await axiod.get(
+    api_base + "/zones/" + zoneId + "/dns_records/",
+    { headers },
+  );
+
+  const dnsId = dns.result.filter((f: Dns) =>
+    f.name == CF_RECORD && f.type == "A"
+  ).map((f: Dns) => {
+    return { id: f.id, ip: f.content };
+  })[0];
+
+  const ip: any = {};
+  (await axiod.get("https://cloudflare.com/cdn-cgi/trace"))
+    .data
+    .split("\n").forEach((f: string) => {
+      const [key, value] = f.split("=");
+      ip[key] = value;
+    });
+
+  console.log("Couldflare IP is", dnsId.ip);
+  console.log("Current IP is " + ip.ip);
+
+  if (dnsId.ip != ip.ip) {
+    console.log("Updating Cloudflare IP");
+    await axiod.put(
+      api_base + "/zones/" + zoneId + "/dns_records/" + dnsId.id,
+      {
+        type: "A",
+        name: "devtron.laspruca.nz",
+        content: ip.ip,
+        ttl: 1,
+      },
+      {
+        headers,
+      },
+    );
+    console.log("Updated IP Address");
+  } else {
+    console.log("IP is the same, skipping update");
+  }
+} catch (ex) {
+  console.log("Caught Exception");
+  console.log(ex.response.data);
+}
+"#;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn create_function(cf_token: String, cf_zone: String, cf_domain: String) -> String {
+    format!(
+        "const CF_TOKEN = {};\nconst CF_ZONE = {};\nconst CF_DOMAIN = {};\n{}",
+        cf_token, cf_zone, cf_domain, PROGRAM
+    )
+}
---
+++ frontend/src/routes/index.svelte
@@ -0,0 +1,13 @@
+<div class="w-full min-h-screen flex justify-center items-center">
+	<div class="text-center rounded-md overflow-hidden">
+		<div class="bg-blue-600 p-5">
+			<h1 class="text-3xl font-bold text-gray-100">CloudFlare AutoUpdate Setup</h1>
+			<p class="text-gray-50">Setup a cloudflare AutoUpdate script of stfp and ssh</p>
+		</div>
+
+		<div class="">
+			<input type="text" id="cf-token" class="focus:border-2 focus:border-gray-400 outline-none" />
+			<label for="cf-token" class="peer-focus:-translate-y-32">CloudFlare Token</label>
+		</div>
+	</div>
+</div>
